2057d273916df851a343f64b180db93e668931ee
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2015-02-03 20:24:10 -0700

    Create Basic.ino

---
+++ CM9IDEv1.0.2/HelloSpoon/examples/Basic.ino
@@ -0,0 +1,15 @@
+#include <HelloSpoon.h>
+
+HelloSpoon robot;
+
+void setup(){
+
+  robot.begin(); // Initialize comm between OpenCM-9.04 and actuators.
+
+  /*Continue writing your code below this line...*/	
+  
+}
+
+void loop(){
+
+}
